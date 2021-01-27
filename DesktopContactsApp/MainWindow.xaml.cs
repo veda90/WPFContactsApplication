430fc278d572ec52fd09c0dd013de1480064054b
veda90 <vedaprakashnayak@gmail.com>
2021-01-27 16:06:31 +0530

    Add project files.

---
+++ DesktopContactsApp/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using DesktopContactsApp.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DesktopContactsApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Contact> contacts;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            contacts = new List<Contact>();
+
+            ReadDatabase();
+        }
+
+        private void NewContact(object sender, RoutedEventArgs e)
+        {
+            NewContactWindow newContactWindow = new NewContactWindow();
+            newContactWindow.ShowDialog();
+           
+            ReadDatabase();
+        }
+
+        void ReadDatabase()
+        {
+            
+            using (SQLite.SQLiteConnection conn = new SQLite.SQLiteConnection(App.databasePath))
+            {
+                conn.CreateTable<Contact>();
+                contacts = (conn.Table<Contact>().ToList()).OrderBy(c => c.Name).ToList();
+            }
+
+            if(contacts != null)
+            {
+                contactsListView.ItemsSource = contacts;
+            }
+        }
+
+        private void Textbox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox searchTextBox = sender as TextBox;
+
+            var filteredList = contacts.Where(c => c.Name.ToUpper().Contains(searchTextBox.Text.ToUpper())).ToList();
+
+            //var filteredList2 = (from c2 in contacts
+            //                    where c2.Name.ToUpper().Contains(searchTextBox.Text.ToUpper())
+            //                    select c2).ToList();
+
+            contactsListView.ItemsSource = filteredList;
+        }
+
+        private void contactsListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ListView listView = sender as ListView;
+            Contact selectedContact = (Contact)listView.SelectedItem;
+
+            if(selectedContact != null)
+            {
+                ContactDetailsWindow contactDetailsWindow = new ContactDetailsWindow(selectedContact);
+                contactDetailsWindow.ShowDialog();
+
+                ReadDatabase();
+            }
+            
+        }
+    }
+}
